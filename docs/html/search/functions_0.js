3d2441efef7b0a7a78d811b9b4099d4e12f2c078
DownGoat <sindre@downgoat.net>
2014-10-29 18:37:44 +0100

    Forgot to generate new docs.

--- docs/html/search/functions_0.js
+++ docs/html/search/functions_0.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['pl_5fcat',['pl_cat',['../plstr_8c.html#ac7619d6d2ab7b5ea840ecd42fe236458',1,'plstr.c']]],
+  ['pl_5fcount',['pl_count',['../plstr_8c.html#a062234d1bae5684a7d02ddd63f793fb8',1,'plstr.c']]],
   ['pl_5fcpy',['pl_cpy',['../plstr_8c.html#a9ac9f1fe19f50bb7b78e7a92e8cffc81',1,'plstr.c']]],
   ['pl_5fendswith',['pl_endswith',['../plstr_8c.html#abd242e26168f062e39e93fbefd2945c4',1,'plstr.c']]],
   ['pl_5fslice',['pl_slice',['../plstr_8c.html#a72b018223239f994ce52ab5daed45504',1,'plstr.c']]],
